ded4e8a2fccbd5fe2e4ac44c97355c74dc29d315
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-06 21:00:35 +0200

    .

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+// - Il computer deve generare 16 numeri casuali da 1 a 100 (bombe).
+// - In seguito deve chiedere all’utente di inserire un numero da 1 a 100 alla volta, se il numero è presente nella lista dei numeri generati la partita termina altrimenti continua chiedendo all’utente un altro numero.
+// - La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// - Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+//INIZIO
+
+var numeroMax = 100;
+var numeroBombe = 16;
+var possibilita = numeroMax - numeroBombe;
+var lista = [];
+var numeriConsentiti = [];
+var utente = 0;
+
+//GENERAZIONE BOMBE
+while (lista.length < numeroBombe){
+    var bomba = numeroRandom(numeroMax);
+    if(! lista.includes(bomba)){    //SE NON C'E' PUSHA DENTRO i NUMERI CHE NON CI SONO
+        lista.push(bomba);
+    }
+}
+console.log("Lista Bombe" , lista);
+
+//CONDIZIONE PER VERIFICARE SE SI PRENDE LA BOMBA
+while( (numeriConsentiti.length < possibilita) && (! lista.includes(utente)  )){
+
+}
+
+
+
+
+
+
+//FUNZIONI
+function numeroRandom(max){
+    return Math.floor( Math.random() * max) + 1;
+}
